ba375788788ff58b3a923c670822f824997e2688
Jay <jaykisan.maheta2302@gmail.com>
2026-01-29 13:01:41 +0530

    allow hosts

--- voicecrm/settings.py
+++ voicecrm/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-m*zbmwh$q#!oy-(akl6b)ngs!@kj27xbg9%1z+&n#+z!j#6ibo
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
